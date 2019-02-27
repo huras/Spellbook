b6d10a9c09d4adea268a4eb6a457828ebff88d42
huras <hurast@gmail.com>
2019-02-27 14:08:28 -0300

    女の語

---
+++ resources/views/Single Pages/WomanLanguage.blade.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <style>
+        body{
+            white-space: pre-line;
+            background-color: black;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+- Substantivos Abstratos (Palavras bolha)
+Liberdade
+Ritmo
+
+- Emoções boas
+
+- Emoções ruins
+
+- Adjetivos (Palavras sentido)
+<span style='color: rgb(201, 38, 38)'> Quente </span>
+
+- Metáforas
+Sentimentos bons trancados num baú > pessoa com meu perfil com a chave > baú e chave se combinam liberando sensações boas
+
+- Fórmula
+
+Componentes :
+Starter : Pergunta, Reportagem de evento
+Emoção (n)
+Sinestesia (n)
+Lugar (n)
+
+Pergunta > Emoção 1 > Sinestesia 1 > Emoção 2 > Sinestesia 2 > Lugar 1
+</body>
